d44e5a66d7f1cad1f6af9278e64d0ba559caa1d2
Cobus Uys <jluysvi@gmail.com>
2025-02-08 15:00:32 +0200

    Setting up the basics.

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export const pages = [
+    ["Home", "/"],
+    ["Services", "/services"],
+    ["About Us", "/about"],
+    ["Portfolio", "/portfolio"],
+    ["Contact", "/contact"]
+];
+
+export const socials = [
+    ["Facebook", "https://www.facebook.com"],
+    ["Twitter", "https://www.twitter.com"],
+    ["Instagram", "https://www.instagram.com"],
+    ["Linkedin", "https://www.linkedin.com"]
+];
+
